648eaf9e7b63959321592c1fac9c279e107a05f3
satnaing <satnaingdev@gmail.com>
2025-10-17 20:36:34 +0700

    feat: configure Google Analytic 4 with PartyTown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
